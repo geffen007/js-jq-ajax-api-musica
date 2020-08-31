8a03dfffccd82705503b1b277b45eaf187b96ae2
Guido <guido.amodio@gmail.com>
2020-08-31 10:50:47 +0200

    array genre

--- main.js
+++ main.js
@@ -19,24 +19,47 @@ $(document).ready(function() {
 
 
 
+
+
     $.ajax({
         'url': 'https://flynn.boolean.careers/exercises/api/array/music',
         'method': 'get',
         'success': function(data) {
-            var dischi = data.response;
-
-            for (var i = 0; i < dischi.length; i++) {
-                var disco= dischi[i];
-
-                var source = $('#entry-template').html();
-                var template = Handlebars.compile(source);
-                var html = template(disco);
-                $('.cds-container').append(html);
-                // console.log(dischi[i].genre);
-            }
+            insertCd (data);
+            insertSelect(data);
         },
         'error': function() {
         alert('Error');
         }
     });
 });
+
+function insertCd (data)  {
+    var dischi = data.response;
+
+    for (var i = 0; i < dischi.length; i++) {
+        var disco = dischi[i];
+
+        var source = $('#entry-template').html();
+        var template = Handlebars.compile(source);
+        var html = template(disco);
+        $('.cds-container').append(html);
+        // console.log(dischi[i].genre);
+    }
+}
+function insertSelect (data)  {
+    var source = $('#template-select').html();
+    var template = Handlebars.compile(source);
+
+    var arrayGenre= [];
+
+    for (var i = 0; i < data.response.length; i++) {
+        var genere = data.response[i].genre;
+        if (!arrayGenre.includes(genere)){
+            arrayGenre.push(genere);
+            var context=data.response[i];
+            var html=template(context);
+            $('.genre').append(html);
+        }
+    }
+}
